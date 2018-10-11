3916f01abbb35f21def73afe43595f15dab9d7b8
Aylin DeBruyne <adebruyne88@gmail.com>
2018-10-11 10:40:51 -0400

    add terrible dad joke array

--- scripts/index.js
+++ scripts/index.js
@@ -6,6 +6,14 @@ console.log("yep, I am here");
 //data attributes go inside of of square brackets
 const triggerElement = document.querySelector("[data-trigger]");
 const outputElement = document.querySelector("[data-output]");
+
+const jokes = [
+  "The shovel was a ground-breaking invention.",
+  "I’ve never gone to a gun range before. I decided to give it a shot!",
+  "I used to work in a shoe recycling shop. It was sole destroying.",
+  "This graveyard looks overcrowded. People must be dying to get in there.",
+  "Want to hear a joke about construction? I'm still working on it."
+];
 // ****************************************************************************
 // function hello() {
 //   console.log("Hello addEventListener");
@@ -16,11 +24,10 @@ const outputElement = document.querySelector("[data-output]");
 //- a function that says what to do when the event happens
 triggerElement.addEventListener("click", function() {
   console.log("Hello addEventListener");
-  outputElement.textContent = "kakakaka";
+  outputElement.textContent = jokes.pop();
 });
 
 // *****************************************************************************
 //#2 Write some text to the page on button click
-
 
 //#3 Pull text from an array of text to write to the page
