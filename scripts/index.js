94277c39b79c94a997c487be3adac41274a36407
Aylin DeBruyne <adebruyne88@gmail.com>
2018-10-11 10:32:28 -0400

    sets output element on click

--- scripts/index.js
+++ scripts/index.js
@@ -1,18 +1,26 @@
 console.log("yep, I am here");
 //Classes are for CSS styling  and "data-attributes" are for JS
 
+// ********************************************************************
 //#1 Make some code run when I click the button
 //data attributes go inside of of square brackets
 const triggerElement = document.querySelector("[data-trigger]");
-
-function hello() {
-  console.log("Hello addEventListener");
-}
+const outputElement = document.querySelector("[data-output]");
+// ****************************************************************************
+// function hello() {
+//   console.log("Hello addEventListener");
+// }
+// *****************************************************************************
 //addEventListener accepts 2 arguments:
 //- a string that identifies the kind of event to listen for
 //- a function that says what to do when the event happens
-triggerElement.addEventListener("click", hello);
+triggerElement.addEventListener("click", function() {
+  console.log("Hello addEventListener");
+  outputElement.textContent = "kakakaka";
+});
 
+// *****************************************************************************
 //#2 Write some text to the page on button click
+
 
 //#3 Pull text from an array of text to write to the page
