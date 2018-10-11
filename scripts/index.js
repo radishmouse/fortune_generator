547284e605fa738eb156aa32d0c56cf1f838aa7b
Aylin DeBruyne <adebruyne88@gmail.com>
2018-10-11 09:49:06 -0400

    add querySelector for button

--- scripts/index.js
+++ scripts/index.js
@@ -2,7 +2,8 @@ console.log("yep, I am here");
 //Classes are for CSS styling  and "data-attributes" are for JS
 
 //#1 Make some code run when I click the button
-const triggerElement = document.querySelector();
+//data attributes go inside of of square brackets
+const triggerElement = document.querySelector('[data-trigger]');
 //#2 Write some text to the page on button click
 
 //#3 Pull text from an array of text to write to the page
