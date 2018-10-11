81e888fe6777ef25b97947a99677ab264c516d84
Chris Aquino <chris.aquino@gmail.com>
2018-10-11 10:31:57 -0400

    sets outputElement's textContent on click

--- scripts/index.js
+++ scripts/index.js
@@ -6,15 +6,19 @@ console.log('yep i am here');
 // #1 Make some code run
 // when I click the button
 const triggerElement = document.querySelector('[data-trigger]');
+const outputElement = document.querySelector('[data-output]');
 
-function hello() {
-    console.log('Hello addEventListener!');
-}
+// function hello() {
+//     console.log('Hello addEventListener!');
+// }
 
 // addEventListener accepts 2 arguments:
 // - a string that identifies the kind of event to listen for
 // - a function that says what to do when the event happens
-triggerElement.addEventListener('click', hello);
+triggerElement.addEventListener('click', function () {
+    console.log('Hello addEventListener!');
+    outputElement.textContent = "la la la la la la lalalalala";
+});
 
 // This works, but is not the most modular approach.
 // triggerElement.onclick = function () {
