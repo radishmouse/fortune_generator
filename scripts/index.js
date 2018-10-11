7d7df449096774df211cb92778760d490e5c1e3b
Chris Aquino <chris.aquino@gmail.com>
2018-10-11 10:11:15 -0400

    adds a hello world

--- scripts/index.js
+++ scripts/index.js
@@ -7,6 +7,22 @@ console.log('yep i am here');
 // when I click the button
 const triggerElement = document.querySelector('[data-trigger]');
 
+function hello() {
+    console.log('Hello addEventListener!');
+}
+
+// addEventListener accepts 2 arguments:
+// - a string that identifies the kind of event to listen for
+// - a function that says what to do when the event happens
+triggerElement.addEventListener('click', hello);
+
+// This works, but is not the most modular approach.
+// triggerElement.onclick = function () {
+//     // all right here in this function
+//     doThing1();
+//     doThing2();
+//     doThing3();
+// }
 
 // #2 Write some text to the
 // page on button click
