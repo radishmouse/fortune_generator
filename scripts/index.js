a7e998e93077004973a7576f8b914fd36dc410f9
Aylin DeBruyne <adebruyne88@gmail.com>
2018-10-11 10:19:34 -0400

    use a function called hello to check if click is working

--- scripts/index.js
+++ scripts/index.js
@@ -3,7 +3,16 @@ console.log("yep, I am here");
 
 //#1 Make some code run when I click the button
 //data attributes go inside of of square brackets
-const triggerElement = document.querySelector('[data-trigger]');
+const triggerElement = document.querySelector("[data-trigger]");
+
+function hello() {
+  console.log("Hello addEventListener");
+}
+//addEventListener accepts 2 arguments:
+//- a string that identifies the kind of event to listen for
+//- a function that says what to do when the event happens
+triggerElement.addEventListener("click", hello);
+
 //#2 Write some text to the page on button click
 
 //#3 Pull text from an array of text to write to the page
